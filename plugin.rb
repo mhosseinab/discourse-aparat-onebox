e1403d8316c59f2641dee0df7ab26d5b1fc90e59
mhosseinab <32487093+mhosseinab@users.noreply.github.com>
2019-02-05 20:57:09 +0100

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,32 +1,84 @@
-# name: Discourse Aparat Onebox
-# about: Adds support for embedding aparat.com videos within Discourse.
+#!/bin/env ruby
+# encoding: utf-8
+# name: Discourse Go7 Onebox
+# about: Adds support for embedding Go7.ir media within Discourse.
 # version: 0.1
 # authors: M H
-# url: https://github.com/mhosseinab/discourse-aparat-onebox
+# url: https://github.com/mhosseinab/discourse-go7-onebox
 
-class Onebox::Engine::AparatOnebox
-	include Onebox::Engine
+require 'net/http'
 
-	REGEX = /^https?:\/\/www.aparat.com\/v\/(\w+)/
-	matches_regexp REGEX
+Onebox = Onebox
 
-	def id
-		@url.match(REGEX)[1]
-	end
-	
-	def to_html
-		%{
-		  <aside class="onebox aparatvideo">
-		    <header class="source">
-		      <a href="https://www.aparat.com/v/#{id}" target="_blank" rel="nofollow noopener">aparat.com</a>
-		    </header>
-		    <article class="onebox-body">
-		      <iframe class="embed-responsive-item" data-src="https://www.aparat.com/video/video/embed/videohash/#{id}/vt/frame" height="360" width="640" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" src="https://www.aparat.com/video/video/embed/videohash/#{id}/vt/frame"></iframe>
-		    </article>
-		    <div class="onebox-metadata">
-		    </div>
-		    <div style="clear: both"></div>
-		  </aside>
-		}
-	end
+  class Onebox::Engine::GoIrOnebox
+  include Onebox::Engine
+  
+  def self.priority
+    0
+  end
+  
+  REGEX = /^https?\:\/\/go7.ir\/i\/(\d*)/
+  matches_regexp REGEX
+
+  def id
+    @url.match(REGEX)[1]
+  end
+  
+  def type
+    res = Net::HTTP.start('go7.ir', '80') {|http|
+      http.get("/i/#{id}")
+    }
+    ext = File.extname(res['location'])
+    if ['.mp4'].include? ext
+      return 'video'
+    elsif ['.mp3','.oga'].include? ext
+      return 'audio'
+    elsif ['.pdf','.doc','.docx'].include? ext
+      return 'documet'
+    elsif ['.jpg','.jpeg','.gif','.png'].include? ext
+      return 'image'
+    else
+      return 'other'
+    end
+  end
+  
+  def to_html
+    content_type = type
+    if content_type == 'audio'
+      return %{
+        <audio controls="">
+          <source src="https://go7.ir/i/#{id}">
+          <a href="https://go7.ir/i/#{id}" rel="noopener">https://go7.ir/i/#{id}</a>
+        </audio>
+      }
+    elsif content_type == 'video'
+      return %{
+        <aside class="onebox video-onebox">
+          <header class="source">
+            <a href="https://go7.ir/i/#{id}" target="_blank" rel="noopener">https://go7.ir/i/#{id}</a>
+          </header>
+          <article class="onebox-body">
+            <video width="100%" height="100%" controls="">
+              <source src="https://go7.ir/i/#{id}">
+              <a href="https://go7.ir/i/#{id}" rel="noopener">https://go7.ir/i/#{id}</a>
+            </video>
+          </article>
+          <div class="onebox-metadata">
+          </div>
+          <div style="clear: both"></div>
+        </aside>
+      }
+    elsif type == 'image'
+      return %{
+        <p><img src="https://go7.ir/i/#{id}" style="max-width:100% !important" class="d-lazyload"></img></p>
+      }
+    else
+      return %{
+        <aside class="onebox">
+          <a href="https://go7.ir/i/#{id}" rel="noopener">https://go7.ir/i/#{id}</a>
+          <div style="clear: both"></div>
+        </aside>
+      }
+    end
+  end
 end 
