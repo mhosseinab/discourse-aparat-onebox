89a012c24a1d34d034e329c3bf232b43e41f4094
mhosseinab <32487093+mhosseinab@users.noreply.github.com>
2020-07-26 04:12:13 +0200

    fix typo

--- plugin.rb
+++ plugin.rb
@@ -1,84 +1,42 @@
 #!/bin/env ruby
 # encoding: utf-8
-# name: Discourse Go7 Onebox
-# about: Adds support for embedding Go7.ir media within Discourse.
+# name: Discourse Aparat Onebox
+# about: Adds support for embedding Aparat.com media within Discourse.
 # version: 0.1
 # authors: M H
-# url: https://github.com/mhosseinab/discourse-go7-onebox
+# url: https://github.com/mhosseinab/discourse-aparat-onebox
 
 require 'net/http'
 
 Onebox = Onebox
 
-  class Onebox::Engine::GoIrOnebox
+  class Onebox::Engine::AparatOnebox
   include Onebox::Engine
   
   def self.priority
     0
   end
   
-  REGEX = /^https?\:\/\/go7.ir\/i\/(\d*)/
+  REGEX = /^https?\:\/\/www\.aparat\.com\/v\/\/(\w*)/
   matches_regexp REGEX
 
   def id
     @url.match(REGEX)[1]
   end
   
-  def type
-    res = Net::HTTP.start('go7.ir', '80') {|http|
-      http.get("/i/#{id}")
-    }
-    ext = File.extname(res['location'])
-    if ['.mp4'].include? ext
-      return 'video'
-    elsif ['.mp3','.oga'].include? ext
-      return 'audio'
-    elsif ['.pdf','.doc','.docx'].include? ext
-      return 'documet'
-    elsif ['.jpg','.jpeg','.gif','.png'].include? ext
-      return 'image'
-    else
-      return 'other'
-    end
-  end
-  
   def to_html
-    content_type = type
-    if content_type == 'audio'
-      return %{
-        <audio controls="">
-          <source src="https://go7.ir/i/#{id}">
-          <a href="https://go7.ir/i/#{id}" rel="noopener">https://go7.ir/i/#{id}</a>
-        </audio>
-      }
-    elsif content_type == 'video'
-      return %{
+    return %{
         <aside class="onebox video-onebox">
           <header class="source">
-            <a href="https://go7.ir/i/#{id}" target="_blank" rel="noopener">https://go7.ir/i/#{id}</a>
+            <a href="https://www.aparat.com/v/#{id}" target="_blank" rel="noopener">https://www.aparat.com/v/#{id}</a>
           </header>
           <article class="onebox-body">
-            <video width="100%" height="100%" controls="">
-              <source src="https://go7.ir/i/#{id}">
-              <a href="https://go7.ir/i/#{id}" rel="noopener">https://go7.ir/i/#{id}</a>
-            </video>
+            <div id="#{id}"><script type="text/JavaScript" src="https://www.aparat.com/embed/#{id}?data[rnddiv]=#{id}&data[responsive]=yes"></script></div>
           </article>
           <div class="onebox-metadata">
           </div>
           <div style="clear: both"></div>
         </aside>
       }
-    elsif type == 'image'
-      return %{
-        <p><img src="https://go7.ir/i/#{id}" style="max-width:100% !important" class="d-lazyload"></img></p>
-      }
-    else
-      return %{
-        <aside class="onebox">
-          <a href="https://go7.ir/i/#{id}" rel="noopener">https://go7.ir/i/#{id}</a>
-          <div style="clear: both"></div>
-        </aside>
-      }
-    end
   end
 end 
